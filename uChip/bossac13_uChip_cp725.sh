1b1b301777c19e309caee2bc834d8a08c0602747
Mike Weiblen <mew@mew.cx>
2022-04-27 23:17:50 -0600

    converting from BAT to bash script

--- uChip/bossac13_uChip_cp725.sh
+++ uChip/bossac13_uChip_cp725.sh
@@ -1,24 +1,25 @@
-REM bossac191_uChip_cp723.bat -- http://mew.cx/ 2022-03-17
-REM Use BOSSAC to flash CircuitPython binary file to Itaca uChip.
+#! /bin/bash -x
+# bossac13_uChip_cp725.bat -- http://mew.cx/ 2022-04-27
+# Use BOSSAC to flash CircuitPython binary file to Itaca uChip.
 @echo off
-REM https://circuitpython.org/board/uchip/
-REM https://github.com/shumatech/BOSSA/releases/tag/1.9.1
-REM https://downloads.circuitpython.org/bin/uchip/en_US/adafruit-circuitpython-uchip-en_US-7.2.3.bin
-REM https://learn.adafruit.com/welcome-to-circuitpython/non-uf2-installation
+# https://circuitpython.org/board/uchip/
+# https://github.com/shumatech/BOSSA/releases/tag/1.9.1
+# https://downloads.circuitpython.org/bin/uchip/en_US/adafruit-circuitpython-uchip-en_US-7.2.3.bin
+# https://learn.adafruit.com/welcome-to-circuitpython/non-uf2-installation
 
-REM The BOSSAC commandline executable.
+# The BOSSAC commandline executable.
 set BOSSAC="C:\Program Files (x86)\BOSSA\bossac.exe"
 
-REM The CircuitPython .bin image file. (_not_ a .uf2 file!)
+# The CircuitPython .bin image file. (_not_ a .uf2 file!)
 set BINFILE=".\adafruit-circuitpython-uchip-en_US-7.2.3.bin"
 
-REM The serial port for uChip.  Edit to match your own.
+# The serial port for uChip.  Edit to match your own.
 set PORT=COM38
 
-REM For M0 boards, which have an 8kB bootloader.  uChip uses this.
+# For M0 boards, which have an 8kB bootloader.  uChip uses this.
 set OFFSET="--offset=0x2000"
-REM For M4 boards, which have a 16kB bootloader.
-REM set OFFSET="--offset=0x4000"
+# For M4 boards, which have a 16kB bootloader.
+# set OFFSET="--offset=0x4000"
 
 echo.
 echo Ensure the uChip is in bootloader mode, and port %PORT% is correct.
@@ -33,14 +34,14 @@ echo Proceed to write?
 pause
 echo.
 
-REM Make it so.
+# Make it so.
 echo Writing to the device.
 echo.
 set WRITE=--erase --write --verify --reset
-REM set WRITE=--erase --write --verify --debug --reset
+# set WRITE=--erase --write --verify --debug --reset
 %BOSSAC% --port=%PORT% %OFFSET% %WRITE% %BINFILE%
 
 echo Finished
 pause
 
-REM eof
+# eof
